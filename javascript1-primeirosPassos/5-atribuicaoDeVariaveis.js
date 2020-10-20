0b6b37319176c94ac5987565c3c451155c87cadc
grynhas <patrick.d.s.reis@gmail.com>
2020-10-20 19:18:24 -0300

    atribuição de valores

---
+++ javascript1-primeirosPassos/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,14 @@
+console.log("trabalhando com atribuição de variáveis");
+
+const idade = 29;
+let nome = "patrick";
+const sobrenome = "Reis";
+
+console.log(nome, sobrenome);
+console.log(`meu nome é ${nome} ${sobrenome}`);
+
+//usando crase ao invés de aspas podemos escrever string com variáveis dentro 
+
+nome = nome + sobrenome;
+
+console.log(nome);
