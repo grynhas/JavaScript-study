f97af8379fa5f41ab5636ef3b7a66bbbaf716c8b
grynhas <patrick.d.s.reis@gmail.com>
2020-10-20 17:35:58 -0300

    primeiros passos com variaveis

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,13 @@
 console.log("hello word");
+console.log("primeiro programa: trabalhando com variáveis");
+
+const idade = 26; 
+
+console. log(idade);
+console. log(idade + 2);
+console. log(idade - 2);
+console. log(idade / 2);
+console. log(idade * 2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
